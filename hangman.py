1d35f0e193dda5f0172de0c824100d34ffa689fc
monchicc <nverming@gmail.com>
2022-09-12 16:43:10 +0300

    updated

--- hangman.py
+++ hangman.py
@@ -3,54 +3,59 @@ import re
 
 
 def main():
-    secretWord = random.choice(
-        ["apple", "Japan", "curious", "onepiece", "fortune"])
+    playAgain = "y"
+    while playAgain == "y":
+        secretWord = random.choice(
+            ["apple", "japan", "curious", "onepiece", "fortune"])
 
-    letterGuessed = ""
+        letterGuessed = ""
 
-    failureCount = len(secretWord)
+        failureCount = len(secretWord)
 
-    print(f"Welcome to the hangman! You have {failureCount} chances to guess!")
+        print(f"Welcome to the hangman! You have {failureCount} chances to fail!")
 
-    while failureCount > 0:
-        guess = input("Enter a letter: ")
-        # to check the input is valid or not
-        while not (re.search("[a-zA-Z]", guess)) or not (len(guess) == 1):
-            print("Incorrect input! Please input one character from a to z.")
-            guess = input("Enter a letter: ")
-
-        else:
-            while guess in letterGuessed:
-                print("You guessed this letter already. Please try another one")
-                guess = input("Enter a letter: ")
+        while failureCount > 0:
+            guess = (input("Enter a letter: ").lower())
+            # to check the input is valid or not
+            while not (re.search("[a-zA-Z]", guess)) or not (len(guess) == 1):
+                print("Incorrect input! Please input one character from a to z.")
+                guess = input("Enter a letter: ").lower()
 
             else:
-                if guess in secretWord:
-                    print(
-                        f"Correct! There is one or more {guess} in the secret word.")
+                while guess in letterGuessed:
+                    print("You guessed this letter already. Please try another one")
+                    guess = input("Enter a letter: ").lower()
 
                 else:
-                    failureCount -= 1
-                    print(
-                        f"incorrect. There is no {guess} in the secret word. {failureCount} turn(s) left.")
+                    if guess in secretWord:
+                        print(
+                            f"Correct! There is one or more {guess} in the secret word.")
 
-                letterGuessed = letterGuessed + guess
-                wrongLetterCount = 0
-
-                for letter in secretWord:
-                    if letter in letterGuessed:
-                        print(f"{letter}", end="")
                     else:
-                        print(f"_", end="")
-                        wrongLetterCount += 1
-                print("\n")
+                        failureCount -= 1
+                        print(
+                            f"incorrect. There is no {guess} in the secret word. {failureCount} turn(s) left.")
 
-                if wrongLetterCount == 0:
-                    print(
-                        f"Congratulations! The secret word was :{secretWord}. You won!")
-                    break
-    else:
-        print(f"Sorry! You lose!")
+                    letterGuessed = letterGuessed + guess
+                    wrongLetterCount = 0
+
+                    for letter in secretWord:
+                        if letter in letterGuessed:
+                            print(f"{letter}", end="")
+                        else:
+                            print(f"_", end="")
+                            wrongLetterCount += 1
+                    print("\n")
+
+                    if wrongLetterCount == 0:
+                        print(
+                            f"Congratulations! The secret word was :{secretWord}. You won!")
+                        break
+        else:
+            print("Sorry! You lose!")
+        playAgain = input("Do you want to play again? Y/N\n").lower()
+
+   
 
 
 if __name__ == "__main__":
